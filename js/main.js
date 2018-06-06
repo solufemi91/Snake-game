0e536e733f1b211e7c1553eba0c81bead51b0e81
Steven.Olufemi-Oluka <Steven.Olufemi-Oluka@J2MINTWKS110070>
2018-06-06 14:22:06 +0100

    added button to get snake moving

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 $(function(event) {
 
   var $snake = $('#snake')
+  var $demo = $('#demo')
+  var $button = $('#myBtn')
 
   function moveSnakeRight() {
     $snake.animate({
       marginLeft: 198 + "px"
-    },10000);
+    },100);
   };
+  function displayDate() {
+      console.log(Date());
+  }
+  //moveSnakeRight();
 
-  moveSnakeRight();
+  //$button.addEventListener("click", displayDate);
+  $button.click(moveSnakeRight);
 
 })

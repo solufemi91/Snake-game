6693ce4f0f12ed8b91e9c7c0df89a2d081bf5940
Steven.Olufemi-Oluka <Steven.Olufemi-Oluka@J2MINTWKS110070>
2018-06-06 15:13:37 +0100

    left arrow key moves snake left

--- js/main.js
+++ js/main.js
@@ -8,23 +8,26 @@ $(function(event) {
   function moveSnakeRight() {
     $snake.animate({
       marginLeft: 198 + "px"
-    },100);
+    },10000);
   };
-  function displayDate() {
-      console.log(Date());
-  }
+
+  function moveSnakeLeft() {
+    $snake.animate({
+      marginLeft: 0 + "px"
+    },1000);
+  };
+
 
   $(document).ready(function(){
     $body.keydown(function(event){
         console.log("Key: " + event.which);
         if (event.which == 39 ) {
           moveSnakeRight();
+        }  else if (event.which == 37 ) {
+          moveSnakeLeft();
         }
     });
   });
 
-
-
-  //$body.keydown(moveSnakeRight);
 
 })
